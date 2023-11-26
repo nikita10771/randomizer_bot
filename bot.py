d5bfb5056478742fac2f30a163575107ffa8618e
nikita10771 <nikita0309p@gmail.com>
2023-11-26 22:41:25 +0300

    bot1

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Здарова маленький ублюдыш, I am BotDvuhDolboebov and always be back.
+Я могу размазать тебя по стенке и разбить тебе ебальник!\
 """)
 
 
